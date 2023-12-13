51c71d5b1103502aadfc110819a17f446287f119
sethuraman-g <sethuraman.ganesan@numentica-ui.com>
2023-12-13 11:48:59 +0530

    Changes

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World !!! ...This is Sethu and I'm learning react from the scratch !!!</h1>
+    </div>
+  );
+}
+
+export default App;
